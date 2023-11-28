3e4f12485834b9ff00281a0f9e146fea6ac8e638
kcbalbus <kc.balbus@gmail.com>
2023-11-28 14:28:00 +0100

    movies class updated for trailers

--- app/src/main/java/com/example/androidlab2/Movie.kt
+++ app/src/main/java/com/example/androidlab2/Movie.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 
-data class Movie(val title: String, val cover: String, val description: String, val scenes: List<String>, val actors: List<String>) {
+data class Movie(val title: String, val cover: String, val description: String, val scenes: List<String>, val actors: List<String>, val trailers: List<String>) {
 
     @Composable
     fun getCoverId (): Int {
@@ -22,5 +22,17 @@ data class Movie(val title: String, val cover: String, val description: String, 
         }
 
         return scenesIds.toList()
+    }
+
+    @Composable
+    fun getTrailderIds (): List<Int> {
+        var trailerIds: MutableList<Int> = mutableListOf()
+        val resources = LocalContext.current.resources
+
+        trailers.forEach { trailer ->
+            trailerIds.add(resources.getIdentifier(trailer, "drawable", LocalContext.current.packageName))
+        }
+
+        return trailerIds.toList()
     }
 }
